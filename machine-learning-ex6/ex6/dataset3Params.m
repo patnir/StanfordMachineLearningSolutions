a91cae26c7573785027f37b48d70b7b62ad93bf3
patnir <rahul20patni@gmail.com>
2016-05-23 19:26:17 -0400

    Add dataset3 implementation

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -16,13 +16,31 @@ sigma = 0.3;
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
 %               validation set. For example, 
-%                   predictions = svmPredict(model, Xval);
+%                   predictions = svmPredict(gaussianKernel, Xval);
 %               will return the predictions on the cross validation set.
 %
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
 
+prediction_min = intmax;
+C_options=[0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigma_options=[0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+
+for i = 1:length(C_options)
+    Cs = C_options(i);
+    for j = 1:length(sigma_options)
+        Ss = sigma_options(j);
+        model = svmTrain(X, y, Cs, @(x1, x2) gaussianKernel(x1, x2, Ss));
+        predictions = svmPredict(model, Xval);
+        prediction_error = mean(double(predictions ~= yval));
+        if prediction_min > prediction_error
+            C = Cs;
+            sigma = Ss;
+            prediction_min = prediction_error;
+        end
+    end
+end
 
 
 
