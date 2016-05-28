7d3aba86dc7e3615cf108cb1e30400296b66513c
patnir <rahul20patni@gmail.com>
2016-05-28 10:15:20 -0400

    Adding regularization to gradient function

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -47,6 +47,10 @@ J = J + (lambda / 2) * (sum(sum((Theta .^ 2))) + sum(sum((X .^ 2))));
 X_grad = (((X * Theta' - Y) .* R) * Theta);
 Theta_grad = (((X * Theta' - Y) .* R)' * X);
 
+% Adding regularization terms
+X_grad = X_grad + lambda * X;
+Theta_grad = Theta_grad + lambda * Theta;
+
 % =============================================================
 
 grad = [X_grad(:); Theta_grad(:)];
